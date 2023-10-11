3cade28de102da13ebd1a8f0315945bea1b6557f
T2203E <huynhldth2202006@fpt.edu.vn>
2023-10-11 23:27:48 +0700

    1

---
+++ src/main/java/com/example/demo/filter/Filter2.java
@@ -0,0 +1,24 @@
+package com.example.demo.filter;
+
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public  class Filter2 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("do filter 2");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
+
---
+++ src/main/java/com/example/demo/servlet/UploadFileServlet.java
@@ -0,0 +1,41 @@
+package com.example.demo.servlet;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.io.IOException;
+import java.io.Writer;
+
+@WebServlet (value = "/upload-file")
+@MultipartConfig
+public class UploadFileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        Writer writer = resp.getWriter();
+        writer.write("<html><body>");
+        writer.write("<form action='upload-file' method='POST'>");
+        writer.write("<input type='file'/>");
+        writer.write("<input type='submit' value='Upload' />");
+        writer.write("</form>");
+        writer.write("</body></html>");
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Part filePart = req.getPart("file");
+        String fileName = getFileName(filePart);
+        filePart.write("part/to/your/upload" + fileName);
+        resp.getWriter().println("file upload successfully");
+    }
+
+}
